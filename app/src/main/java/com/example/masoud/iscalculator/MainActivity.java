f69484039a4d36dec2709a407d03027f89aabd5e
Mr.Robot <yusufzk78@gmail.com>
2018-02-20 22:28:31 +0330

    added four operators

--- app/src/main/java/com/example/masoud/iscalculator/MainActivity.java
+++ app/src/main/java/com/example/masoud/iscalculator/MainActivity.java
@@ -6,91 +6,138 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
-  TextView txt_zero, txt_0ne, txt_two, txt_three, txt_four, txt_five, txt_six, txt_siven, txt_eight, txt_nine, txt_plus, txt_minus, txt_multiplier, txt_devide, txt_delete, txt_equal;
-  TextView txt_result, txt_double_zero, txt_clear, txt_ceremony, txt_ce, txt_dot, txt_step, txt_power, txt_remainder;
-
-
-  int nr1, nr2;
-  String old , New;
-
-  boolean plus, minus, multi, div;
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-
-    txt_result = (TextView) findViewById(R.id.textView26);
-    txt_step = (TextView) findViewById(R.id.textView25);
-
-    txt_clear = (TextView) findViewById(R.id.txt_clear);
-    txt_plus = (TextView) findViewById(R.id.txt_plus);
-    txt_equal = (TextView) findViewById(R.id.txt_equal);
+    TextView txt_zero, txt_0ne, txt_two, txt_three, txt_four, txt_five, txt_six, txt_siven, txt_eight, txt_nine, txt_plus, txt_minus, txt_multiplier, txt_devide, txt_delete, txt_equal;
+    TextView txt_result, txt_double_zero, txt_clear, txt_ceremony, txt_ce, txt_dot, txt_step, txt_power, txt_remainder;
 
 
+    String Click;
+    String old, New;
 
-    txt_clear.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        txt_result.setText("");
-      }
-    });
-    txt_plus.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        String oldValue = txt_result.getText().toString();
-        txt_result.setText(oldValue + "+");
-      }
-    });
+    boolean plus, minus, multi, div;
 
-    txt_equal.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt_result = (TextView) findViewById(R.id.textView26);
+        txt_step = (TextView) findViewById(R.id.textView25);
+
+        txt_clear = (TextView) findViewById(R.id.txt_clear);
+        txt_plus = (TextView) findViewById(R.id.txt_plus);
+        txt_equal = (TextView) findViewById(R.id.txt_equal);
+        txt_minus = (TextView) findViewById(R.id.txt_minus);
+        txt_multiplier = (TextView) findViewById(R.id.txt_multiplier);
+        txt_devide = (TextView) findViewById(R.id.txt_devide);
+
+
+
+        txt_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt_result.setText("");
+            }
+        });
+        txt_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String oldValue = txt_result.getText().toString();
+                txt_result.setText(oldValue + "+");
+            }
+        });
+        txt_devide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String oldValue = txt_result.getText().toString();
+                txt_result.setText(oldValue + "/");
+            }
+        });
+        txt_multiplier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String oldValue = txt_result.getText().toString();
+                txt_result.setText(oldValue + "*");
+            }
+        });
+        txt_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String oldValue = txt_result.getText().toString();
+                txt_result.setText(oldValue + "-");
+            }
+        });
+
+        txt_equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                    Method_All();
+            }
+        });
+
+    }
+
+    private void Method_All()
+    {
         String oldValue = txt_result.getText().toString();
         int findPlus = oldValue.indexOf("+");
-        if(findPlus != -1)
-        {
-          old = oldValue.substring(0,findPlus);
-          New = oldValue.substring(findPlus+1 , oldValue.length());
-          int num1 = Integer.parseInt(old);
-          int num2 = Integer.parseInt(New);
-
-          txt_step.setText(""+(num1+num2));
-
-
+        if(findPlus != -1) {
+            old = oldValue.substring(0, findPlus);
+            New = oldValue.substring(findPlus + 1, oldValue.length());
+            int num1 = Integer.parseInt(old);
+            int num2 = Integer.parseInt(New);
+            txt_step.setText("" + (num1 + num2));
         }
 
+        int findMines = oldValue.indexOf("-");
+        if(findMines != -1) {
+            old = oldValue.substring(0, findMines);
+            New = oldValue.substring(findMines + 1, oldValue.length());
+            int num1 = Integer.parseInt(old);
+            int num2 = Integer.parseInt(New);
+            txt_step.setText("" + (num1 - num2));
+        }
 
-      }
-    });
+        int findMultiplay = oldValue.indexOf("*");
+        if(findMultiplay != -1) {
+            old = oldValue.substring(0, findMultiplay);
+            New = oldValue.substring(findMultiplay + 1, oldValue.length());
+            int num1 = Integer.parseInt(old);
+            int num2 = Integer.parseInt(New);
+            txt_step.setText("" + (num1 * num2));
+        }
 
-  }
-
-  public void onClick(View view)
-  {
-    int length = txt_result.length();
-    if(length <= 14)
-    {
-
-      int getTagNum = Integer.parseInt(view.getTag().toString());
-      old = view.getTag().toString();
-      appendNumerToTextView(getTagNum);
+        int findDevided = oldValue.indexOf("/");
+        if(findDevided != -1) {
+            old = oldValue.substring(0, findDevided);
+            New = oldValue.substring(findDevided + 1, oldValue.length());
+            int num1 = Integer.parseInt(old);
+            int num2 = Integer.parseInt(New);
+            txt_step.setText("" + (num1 / num2));
+        }
 
     }
-    else
+
+    public void onClick(View view)
     {
-      return;
+
+        int length = txt_result.length();
+        if (length <= 14)
+        {
+            int getTagNum = Integer.parseInt(view.getTag().toString());
+            Click = view.getTag().toString();
+            appendNumerToTextView(getTagNum);
+        } else {
+            return;
+        }
     }
-  }
 
-  private void appendNumerToTextView(int getTagNum) {
-
-    String oldValue = txt_result.getText().toString();
-    txt_result.setText(oldValue + getTagNum);
-  }
+    private void appendNumerToTextView(int getTagNum) {
+        String oldValue = txt_result.getText().toString();
+        txt_result.setText(oldValue + getTagNum);
+    }
 
 }
